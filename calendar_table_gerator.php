c3266b94e5a4f6c05e78c74aa3796d6f30486791
Craig <CraigDavison@users.noreply.github.com>
2017-01-27 14:05:48 +0000

    Updated with Create Table
    This new updated version, looks for the relevent table and if it does not exist, creates it before populating it.

--- calendar_table_gerator.php
+++ calendar_table_gerator.php
@@ -1,73 +1,89 @@
 <?php
 date_default_timezone_set('Europe/London');
 set_time_limit (360000);
-
-// this is the authors initialisation script which will be used to establish database connection.
-// You will have your own way of doing this.
 include_once '../core/init.php';
-
-// Authors method for connecting to DB
 $db = new DB();
-// Table to update
-$table = 'tbl_calendar';
+$table = 'tbl_dates_calendar';
+//$days = 36500;
 
-// Start date (comment out as required). The first option is a defined by you.
-// The second method finds the last date in the table.
-$Start = '2036-10-07';
-$Start = $db->query('SELECT max(`cal_date`) AS s_date FROM `tbl_calendar`')->results()[0]->s_date;
+$createSQL = 'CREATE TABLE '.$table.' (
+               cal_date date NOT NULL,
+               cal_year int(4) NOT NULL,
+               cal_isLeapYear int(1) NOT NULL,
+               cal_day_of_week_string varchar(10) COLLATE utf8_unicode_ci NOT NULL,
+               cal_day_of_month int(2) NOT NULL,
+               cal_day_of_week int(2) NOT NULL,
+               cal_month int(2) NOT NULL,
+               cal_week int(2) NOT NULL,
+               cal_day_of_year int(3) NOT NULL,
+               cal_is_weekday int(1) NOT NULL,
+               cal_x_weekday_of_month int(2) NOT NULL
+             ) ENGINE=InnoDB DEFAULT CHARSET=utf8 COLLATE=utf8_unicode_ci';
+// Check to see if table exists
+$sql = 'SHOW TABLES LIKE "'.$table.'"';
 
-$date = new DateTime($Start); 
-// Regardless of which method you used to define your start date
-// the following adds 1 day to that, if you used the query method you need to keep this
-// if you defined it yourself you can remove it or define the start as one day earlier
-$date = $date->add(new DateInterval('P1D'));
 
-// End date will either be static or x days after start date
+// count the number of tables returned with the table name
+$tableExists = $db->query($sql)->count();
+// if is 0 then table does not exist
+if(!$tableExists) {
+  $createTable = $db->query($createSQL);
+}
+// check table was created and if populate table.
+if($db->query($sql)){
+  if(!isset($_GET['start'])){
+    $start = $db->query('SELECT max(cal_date) AS s_date FROM '.$table)->results()[0]->s_date;
+    if(!$start){
+      $start = date('Y-m-d',time());
+    }
+  } else {
+    $start = $_GET['start'];
+  }
+  $date = new DateTime($start);
+  $date = $date->add(new DateInterval('P1D'));
+  if(!isset($_GET['end'])){
+    $end = new DateTime('2050-12-31');
+  } else {
+    $end = $_GET['end'];
+  }
+  //$end = $end->add(new DateInterval('P'.$days.'D'));
 
-// static end date
-$end = new DateTime('2099-12-31');
+  $y = 0;
+  while( $date != $end) {
+      if($date->format('Y') != $y){
+          echo $date->format('Y');
+      }
 
-// x days after start
-//$days = 3650; // 10 years
-//$end = $end->add(new DateInterval('P'.$days.'D'));
+      $year = $date->format('Y');
+      $month = $date->format('n');
+      $day = $date->format('N');
+      $Date = $date->format('Y-m-d');
+      $sql = 'SELECT * FROM '.$table.' WHERE cal_year = ? AND cal_month = ? AND cal_day_of_week = ? AND cal_date < ?';
+      // nthOfDay = for example 1st Monday of Month 2nd Friday of Month or 3rd Sunday
+      // To do this I establis the current day and then query the table to find
+      // out how many times that day has occured in the current month
+      $nthOfDay = $db->query($sql,array($year,$month,$day,$Date))->count() + 1;
 
-// loop through each day from the start date until is equal to the end date
-while( $date != $end) {
+      $fields = array('cal_date' => $date->format('Y-m-d'),
+                      'cal_year' => $date->format('Y'),
+                      'cal_isLeapYear' => $date->format('L'),
+                      'cal_day_of_week_string' => $date->format('D'),
+                      'cal_day_of_month' => $date->format('d'),
+                      'cal_day_of_week' => $date->format('N'),
+                      'cal_month' => $date->format('n'),
+                      'cal_week' => $date->format('W'),
+                      'cal_day_of_year' => $date->format('z')+1,
+                      'cal_is_weekday' => ($date->format('N') < 6 ? 1 : 0),
+                      'cal_x_weekday_of_month' => $nthOfDay);
 
-    // nthOfDay = for example 1st Monday of Month 2nd Friday of Month or 3rd Sunday
-    // To do this I establis the current day (Mon, Tue etc) and then query the table to find 
-    // out how many times that day has occured in the current month of the current year.
-    $year = $date->format('Y');
-    $month = $date->format('n');
-    $day = $date->format('N');
-    $Date = $date->format('Y-m-d');
-    
-    // SQL required for above query
-    $sql = 'SELECT * FROM tbl_calendar WHERE cal_year = ? AND cal_month = ? AND cal_day_of_week = ? AND cal_date < ?';
-    
-    // Query database. How you query the database may be different. I have not included all the code that I use for 
-    // doing that as we all have different method but all I am doing here is using a prepared statement to query the
-    // database to establish how many times the current day has occured in the current month.
-    $nthOfDay = $db->query($sql,array($year,$month,$day,$Date))->count() + 1;
-    
-    // This array is used by my insert function and holds all the field names and values to be inserted
-    $fields = array('cal_date' => $date->format('Y-m-d'),
-                    'cal_year' => $date->format('Y'),
-                    'cal_isLeapYear' => $date->format('L'),
-                    'cal_day_of_week_string' => $date->format('D'),
-                    'cal_day_of_month' => $date->format('d'),
-                    'cal_day_of_week' => $date->format('N'),
-                    'cal_month' => $date->format('n'),
-                    'cal_week' => $date->format('W'),
-                    'cal_day_of_year' => $date->format('z')+1,
-                    'cal_is_weekday' => ($date->format('N') < 6 ? 1 : 0),
-                    'cal_x_weekday_of_month' => $nthOfDay);  
-    
-    // Again this is my method of interacting with the database but all it is doing is inserting all the
-    // the values in the the table.... you will likely have your own method of doing this. 
-    $insert = $db->insert($table, $fields);
+      $insert = $db->insert($table, $fields);
 
-    $date = $date->add(new DateInterval('P1D'));    
- } // end of while loop
+      $date = $date->add(new DateInterval('P1D'));
+      $y = $date->format('Y');
 
-echo 'done!';
+   }
+  echo 'Complete!';
+} else {
+  echo 'table does not exist and it could not be created';
+}
+?>
